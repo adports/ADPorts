769b2ae6ffd4c5670e5756b7d0c12a61d21ed9a1
praveen <praveen.rangushbe@adports.ae>
2018-11-28 12:07:34 +0530

    worked on login page api & reqistration api is updated

---
+++ src/app/login/user.service.ts
@@ -0,0 +1,39 @@
+// import {Injectable} from '@angular/core';
+// import {HttpClient, HttpHeaders} from '@angular/common/http';
+//
+//
+// @Injectable({
+//     providedIn: 'root'
+// })
+// export class UserService {
+//
+//     header;
+//     httpOptions;
+//
+//
+//     constructor(public http: HttpClient) {
+//     //     this.httpOptions = {
+//     //         headers: new HttpHeaders({
+//     //             'Content-Type': 'application/json',
+//     //             'Access-Control-Allow-Origin': '*',
+//     //         })
+//     //     };
+//     //     // this.header = new HttpHeaders();
+//     //     // this.header = this.header.append('Content-Type', 'text/xml');
+//     //     // this.header = this.header.append('Accept', 'text/xml');
+//     //     // this.header = this.header.append('Access-Control-Allow-Origin', '*');
+//     // }
+// //     validateUsers(userData) {
+// //         alert( userData.username);
+// // //        // alert(userData);
+// // //         const postedData = `<body> <username>xavier.fernando</username>
+// // //     <password>Xcalvyn81</password>
+// // // </body>`;
+// //         return this.http.post('http://adports.ae/e-Pass/webservices/ValidateUser', userData.username , this.httpOptions);
+// //     }
+//
+// }
+//
+//
+//
+//
---
+++ src/app/registration/registration.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+    private data;
+
+    constructor(public http: HttpClient) {
+    }
+    newRegistration(data) {
+        alert('hi')
+        let headers: HttpHeaders = new HttpHeaders();
+        console.log(this.data);
+        headers = headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        return this.http.post('http://172.21.111.48:92/WSTestPass/WSePass.asmx/SubmitUserRegistration', {data}, {headers});
+    }
+}
